4b96aca65132f9d53fdee0bbb0a69b3150246906
Bigdady14 <bigexchangecripto@gmail.com>
2025-07-23 07:21:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Dummy simulation for dynamic values
+document.getElementById("txCount").innerText = (Math.random() * 1_000_000 + 500_000).toFixed(0);
+document.getElementById("nodes").innerText = Math.floor(Math.random() * 100 + 100);
+document.getElementById("blockHeight").innerText = (Math.random() * 100_000 + 300_000).toFixed(0);
